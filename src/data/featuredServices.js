e98a0c8f1b678e9613a457674cd6b36f7974afc6
musteey2323 <muhammadalmustapha65@gmail.com>
2025-09-25 23:12:03 +0100

    new update

--- src/data/featuredServices.js
+++ src/data/featuredServices.js
@@ -4,7 +4,7 @@ import service2 from "../assets/images/service2.jpg";
 import service3 from "../assets/images/service3.jpg";
 
 export const featuredServices = [
-  { title: "Short let Apartments", image: service1, link: "/listings?q=accommodation" },
+  { title: "Short Let Apartments", image: service1, link: "/listings?q=accommodation" },
   { title: "Vehicle Rentals", image: service2, link: "/listings?q=mobility" },
   { title: "Private Jet Charters", image: service3, link: "/listings?q=security" },
 ];
